d45d792d23aed37358732f2765986e24c59cb4e6
soler2000 <awilde76@gmail.com>
2025-08-31 10:40:11 +0100

    Install libnice (gstreamer1.0-nice) for WebRTC ICE

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,7 +12,7 @@ sudo apt-get -y install \
   python3-gi python3-gi-cairo gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 \
   gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
   gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl \
-  gstreamer1.0-libcamera
+  gstreamer1.0-libcamera gstreamer1.0-nice
 
 sudo apt-get -y install v4l-utils
 sudo usermod -a -G video "$USER"
